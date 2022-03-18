d1df7a1288448137591be3b2c55bdf206e7d165d
Ziad Azab <100707085+Azab18@users.noreply.github.com>
2022-03-18 15:56:19 +0300

    done

---
+++ ios-cw-4/MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var name = "ziad"
+  var favouriteNumber = 5
+  var hobby = "boxing"
+  
+  
+print("hi my name is \(name) and i love playing \(hobby) and my fav no is \(favouriteNumber)")
+
